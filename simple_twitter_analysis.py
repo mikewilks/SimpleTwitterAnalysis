981854da1b40b7e46e968b8622cdd4ae4ff108c5
Mike Wilks <mike@wilks.org>
2020-10-02 18:22:06 +0100

    Implemented keyword check but needs get_user replaced with lookup_user to stop rate limiting

--- simple_twitter_analysis.py
+++ simple_twitter_analysis.py
@@ -30,10 +30,24 @@ def load_ids_to_analyse(filename):
     return ids_list
 
 
-def analyse(tweepy_api, ids):
+def analyse(tweepy_api, ids, keyword_to_check):
     """Do the analysis and return. """
-
-    analysed_data_item = ['Name', 'Twitter ID', 'Followers', 'Tweets', 'Last Tweet', 'Last Checked']
+    if keyword_to_check == '':
+        analysed_data_item = ['Name',
+                              'Twitter ID',
+                              'Followers',
+                              'Tweets',
+                              'Last Tweet',
+                              'Last Checked']
+    else:
+        analysed_data_item = ['Name',
+                              'Twitter ID',
+                              'Followers',
+                              'Keyword Followers',
+                              'Keyword % of followers',
+                              'Tweets',
+                              'Last Tweet',
+                              'Last Checked']
 
     analysed_data = list()
 
@@ -47,15 +61,57 @@ def analyse(tweepy_api, ids):
         print(i)
 
         # Get the results
-        analysed_data_item = [user_to_check.name,
-                              i,
-                              user_to_check.followers_count,
-                              user_to_check.statuses_count,
-                              user_to_check.status.created_at.strftime('%d/%m/%Y'),
-                              datetime.date.today()]
+        if keyword_to_check == '':
+            analysed_data_item = [user_to_check.name,
+                                  i,
+                                  user_to_check.followers_count,
+                                  user_to_check.statuses_count,
+                                  user_to_check.status.created_at.strftime('%d/%m/%Y'),
+                                  datetime.date.today()]
+        else:
+            # Get their followers and check for the presence
+            # of the keyword in it
+            followers = get_followers(tweepy_api, user_to_check)
+            instances_of_keyword = count_keywords_in_profile(tweepy_api,
+                                                             followers,
+                                                             keyword_to_check)
+
+            # Build the data object to return
+            analysed_data_item = [user_to_check.name,
+                                  i,
+                                  user_to_check.followers_count,
+
+                                  instances_of_keyword,
+                                  user_to_check.followers_count / instances_of_keyword,
+                                  user_to_check.statuses_count,
+                                  user_to_check.status.created_at.strftime('%d/%m/%Y'),
+                                  datetime.date.today()]
+
         analysed_data.append(analysed_data_item)
     return analysed_data
 
+
+def count_keywords_in_profile(tweepy_api, list_of_users, keyword_to_check):
+    """Returns the number profiles containing the keyword in
+    the list_of_users and returns it."""
+    count = 0
+    # Iterating one at a time over the list of users will
+    # get rate-limited by the API very quickly better to
+    # use lookup_users not get_user
+    for user_id in list_of_users:
+        user_object = tweepy_api.get_user(user_id)
+        if keyword_to_check in user_object.description:
+            count += 1
+    print(f"{user_object.name} has {count} keyword followers")
+    return count
+
+
+def get_followers(tweepy_api, twitter_handle):
+    """Returns a list of followers."""
+    followers_list = []
+    for follower in tweepy.Cursor(tweepy_api.followers_ids, id=twitter_handle).items():
+        followers_list.append(follower)
+    return followers_list
 
 # Load the parameters from the json file
 params = load_parameters()
@@ -71,7 +127,7 @@ api = tweepy.API(auth_token)
 ids_to_check = load_ids_to_analyse(params['file_of_ids'])
 
 # Do the analysis
-results = analyse(api, ids_to_check)
+results = analyse(api, ids_to_check, params['profile_keyword'])
 
 # Open the file for writing to
 # and write the data out to a csv file
